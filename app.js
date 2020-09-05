b0f1b01f4d460e8366e5a546965a845048281cae
Indrek Lasn <lasnindrek@gmail.com>
2020-09-05 22:38:50 +0300

    demo

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-const AdminBro = require("admin-bro");
-const AdminBroExpress = require("@admin-bro/expressjs");
+const express = require('express')
+const app = express()
 
 const adminBro = new AdminBro({
   databases: [],
-  rootPath: "/admin"
-});
+  rootPath: '/admin',
+})
 
-var listener = app.listen(8080, function () {
-  console.log("Listening on port " + listener.address().port);
-});
+const router = AdminBroExpress.buildRouter(adminBro)
+
+app.use(adminBro.options.rootPath, router)
+
+app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'))
